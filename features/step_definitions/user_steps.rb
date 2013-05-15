5d7efe5995477420d89b4d04a028ab6cd222c9fa
mixergtz <mixergutierrez@gmail.com>
2013-05-14 19:41:17 -0500

    General corrections made starting 7.3 section

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,8 +1,10 @@
 Given /^there are the following users:$/ do |table|
 	table.hashes.each do |attributes|
-		unconfirmed = attributes.delete("unconfirmed") == "true"
-		@user = User.create!(attributes)
-		@user.confirm! unless unconfirmed
+	  unconfirmed = attributes.delete("unconfirmed") == "true"
+      admin = attributes.delete("admin") == "true"
+      @user = User.create!(attributes)
+      @user.admin = admin
+      @user.confirm! unless unconfirmed
 	end
 end
 
