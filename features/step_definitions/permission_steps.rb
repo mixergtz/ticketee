b3ea1ec7f8bcbff0a6b07f8155407cd82e34a08d
mixergtz <mixergutierrez@gmail.com>
2013-05-31 11:37:13 -0500

    Restricting creating tickets to only users who have permissions to do it

--- features/step_definitions/permission_steps.rb
+++ features/step_definitions/permission_steps.rb
@@ -1,7 +1,20 @@
-Given /^"([^"]*)" can view the "([^"]*)" project$/ do |user, project|
+permission_step = /^"([^"]*)" can ([^"]*?) ([o|i]n)?\s?the "([^"]*)" project$/
+Given permission_step do |user, permission, on, project|
+create_permission(user, find_project(project), permission)
+end
+
+
+
+
+def create_permission(email, object, action)
 	@permission = Permission.create!
-	@permission.user = User.find_by_email!(user)
-	@permission.thing = Project.find_by_name!(project)
-	@permission.action = "view"
+	@permission.user = User.find_by_email!(email)
+	@permission.thing = object
+	@permission.action = action
 	@permission.save
 end
+
+def find_project(name)
+	Project.find_by_name!(name)
+end
+
